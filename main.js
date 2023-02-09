aac8de00815d551d7478258b1c2b46f150982c40
edgarkfc <edgar.castro@kfc.com.ve>
2023-02-09 16:30:14 -0400

    cambios en archivos

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ const desktopMenu = document.querySelector('.desktop-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
+
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
